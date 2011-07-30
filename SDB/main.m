4a969cc2a90b0a62e2119a2c9b92e511935888f1
Brandon Smith <brcosm@gmail.com>
2011-07-30 15:55:32 -0700

    Initial Commit

---
+++ SDB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SDB
+//
+//  Created by Brandon Smith on 7/30/11.
+//  Copyright 2011 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDBAppDelegate class]));
+    }
+}
